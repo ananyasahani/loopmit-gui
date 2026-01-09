5c5835e0c00eacb659fe5cc6916b0a94f4c5e864
ananyasahani <ananya4.mitmpl2023@learner.manipal.edu>
2026-01-09 16:44:53 +0530

    changes made in SensorMetrics

--- src/app/components/SensorMetrics.tsx
+++ src/app/components/SensorMetrics.tsx
@@ -36,9 +36,30 @@ export function SensorMetrics() {
     },
     {
       label: 'bno_health',
-      value: `${sensorData.bno_health.toFixed(1)}mm`,
+      value: `${sensorData.bno_health.toFixed(1)}`,
       icon: <Gauge className="size-6" />,
       color: 'text-red-500',
+      bgColor: 'bg-green-500/10',
+    },
+    {
+      label: 'voltage_health',
+      value: `${sensorData.voltage_health.toFixed(1)}`,
+      icon: <Gauge className="size-6" />,
+      color: 'text-blue-500',
+      bgColor: 'bg-green-500/10',
+    },
+    {
+      label: 'temp_health',
+      value: `${sensorData.temp_health.toFixed(1)}`,
+      icon: <Gauge className="size-6" />,
+      color: 'text-purple-500',
+      bgColor: 'bg-green-500/10',
+    },
+    {
+      label: 'lidar_health',
+      value: `${sensorData.lidar_health.toFixed(1)}`,
+      icon: <Gauge className="size-6" />,
+      color: 'text-yellow-500',
       bgColor: 'bg-green-500/10',
     },
   ];
